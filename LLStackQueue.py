8dcb4ce3c299c1b02efd75c04bfc06157cd6af4c
shullmb <shullmb@gmail.com>
2018-07-26 08:40:42 -0700

    working on linked list stack and queue - queue working

---
+++ LLStackQueue.py
@@ -0,0 +1,58 @@
+from LinkedList import Node
+from LinkedList import LinkedList
+
+class Stack:
+  def __init__(self):
+    self.store = LinkedList()
+
+  def push(self, data):
+    self.store.add(data)
+    return self.store
+  
+  def pop(self):
+    
+    pass
+  
+  def print_stack(self):
+    self.store.print_list()
+
+stack = Stack()
+
+stack.push("one")
+stack.push("two")
+
+# stack.print_stack()
+
+class Queue:
+  def __init__(self):
+    self.store = LinkedList()
+  
+  def enqueue(self, data):
+    self.store.add(data)
+    return self.store
+  
+  def dequeue(self):
+    self.store.delete_at_head()
+
+  def print_queue(self):
+    self.store.print_list()
+
+
+queue = Queue()
+queue.enqueue('one')
+queue.enqueue('two')
+queue.enqueue('three')
+queue.enqueue('four')
+queue.enqueue('five')
+
+queue.dequeue()
+queue.print_queue()
+queue.dequeue()
+queue.print_queue()
+queue.dequeue()
+queue.print_queue()
+queue.dequeue()
+queue.print_queue()
+
+test= queue.dequeue()
+print(test)
---
+++ LinkedList.py
@@ -0,0 +1,90 @@
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next_node = None
+
+class LinkedList:
+  def __init__(self):
+    self.head = None
+
+  def add(self, data):
+    # Create a new Node and stick it in the list
+    node = Node(data)
+    if self.head == None:
+      # the list is empty
+      self.head = node
+    else:
+      current = self.head
+      while current.next_node != None:
+        current = current.next_node
+      current.next_node = node
+
+  def delete(self, data):
+    if self.head == None:
+      return False
+    current = self.head
+    if current.data == data:
+      # we need to delete the head
+      temp_node = current.next_node
+      current.next_node = None
+      self.head = temp_node
+    else:
+      while current.next_node != None:
+        previous = current
+        current = current.next_node
+        if current.data == data:
+          temp_node = current.next_node
+          previous.next_node = temp_node
+          current.next_node = None
+          return True
+      return False
+  
+  def delete_at_head(self):
+    if self.head == None:
+      return False
+    else:
+      current = self.head
+      data = self.head.data
+      temp_node = current.next_node
+      current.next_node = None
+      self.head =temp_node
+      return data
+
+  def insert_before(self, n, data):
+    node = Node(data)
+    if n == 0:
+      # insert before the head and become the new head
+      temp_node = self.head
+      self.head = node
+      self.head.next_node = temp_node
+    else:
+      current = self.head
+      for i in range(0,n):      
+        previous = current
+        current = current.next_node
+        if current == None:
+          previous.next_node = node
+          return True
+      temp_node = previous.next_node
+      previous.next_node = node
+      node.next_node = temp_node
+
+
+  def print_list(self):
+    if self.head != None:
+      current = self.head
+      print(self.head.data)
+      while current.next_node != None:
+        current = current.next_node
+        print(current.data)
+
+ll = LinkedList()
+
+ll.add("one")
+ll.add("two")
+ll.add("three")
+ll.add("four")
+ll.add("five")
+
+test = ll.delete_at_head()
+print(test) # returns payload
